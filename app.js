7f4da19e5a16e3923514fe26c91138e5ce9398de
Abdul Wasay <144446656+wasaydev-1@users.noreply.github.com>
2025-07-21 21:53:55 -0700

    unit testing

--- app.js
+++ app.js
@@ -4,25 +4,23 @@ const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
-
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
-app.use(cors())
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+// Simplified MongoDB connection - the URI already contains all needed info
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://wasay:wasay%40654@cluster0.5vx6n24.mongodb.net/solar?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
-    }
-})
+}).then(() => {
+    console.log("MongoDB Connection Successful");
+}).catch((err) => {
+    console.log("MongoDB Connection Error: " + err);
+});
 
 var Schema = mongoose.Schema;
 
@@ -34,54 +32,53 @@ var dataSchema = new Schema({
     velocity: String,
     distance: String
 });
+
 var planetModel = mongoose.model('planets', dataSchema);
 
-
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
+app.post('/planet', function(req, res) {
+    // console.log("Received Planet ID " + req.body.id)
     planetModel.findOne({
         id: req.body.id
     }, function(err, planetData) {
         if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
+            console.log("Error fetching planet data:", err);
+            res.status(500).send("Error in Planet Data");
+        } else if (!planetData) {
+            res.status(404).send("Planet not found. We only have 9 planets and a sun. Select a number from 0 - 9");
         } else {
             res.send(planetData);
         }
-    })
-})
+    });
+});
 
-app.get('/',   async (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
-
-app.get('/os',   function(req, res) {
+app.get('/os', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
         "env": process.env.NODE_ENV
     });
-})
+});
 
-app.get('/live',   function(req, res) {
+app.get('/live', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "live"
     });
-})
+});
 
-app.get('/ready',   function(req, res) {
+app.get('/ready', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "ready"
     });
-})
+});
 
 app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
-
+    console.log("Server successfully running on port - " + 3000);
+});
 
 module.exports = app;
